6ac2f25b9115634bf576500119e71d4885b3f387
Jana Novotná <jana.posedelova@gmail.com>
2020-06-20 16:49:21 +0200

    Upravené styly na LandingPage

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -4,53 +4,72 @@
 
     <div class="hlavni__obsah">
       <h1 class="hidden">Dárky bez starostí</h1>
-      <p>Vítejte na stránkách, které vám pomohou se starostmi s dárky.</p>
+      <p class="claim">Stránky, které vám pomohou se starostmi s dárky.</p>
       <h2>Jak to funguje?</h2>
       <div class="how_it_works">
         <div class="description">
-          <img src="assets/img/present.png" alt="Dárek" class="icon" />
-          <p>Vytvořte si seznam dárků.</p>
           <img src="assets/img/list.png" alt="Seznam" class="des_icon" />
+          <p>Vytvořte si seznam dárků.</p>
         </div>
         <div class="description">
-          <img src="assets/img/present.png" alt="Dárek" class="icon" />
-          <p>Pošlete seznam svým přátelům.</p>
           <img src="assets/img/mail.png" alt="Obálka" class="des_icon" />
-          <p>Přátele si zamluví dárek, který vám chtějí pořídit.</p>
+          <p>
+            Pošlete seznam svým přátelům.<br />
+            Přátele si na seznamu zamluví dárek, který vám chtějí pořídit.
+          </p>
         </div>
         <div class="description">
-          <img src="assets/img/present.png" alt="Dárek" class="icon" />
-          <p>Těšte se na to, co dostane za dárky!</p>
           <img
             src="assets/img/openedGift.png"
             alt="Otevřený dárek"
             class="des_icon"
           />
+          <p>Těšte se na to, co dostane za dárky!</p>
         </div>
       </div>
-      <router-link class="main__button" to="/muj_seznam/cwYzBFH0XQ6PQ2fym8pk"
+      <router-link
+        class="main__button add__button"
+        to="/muj_seznam/cwYzBFH0XQ6PQ2fym8pk"
         >Zadat nové přání</router-link
       >
     </div>
-
-    <wishPage />
   </div>
 </template>
 
 <script>
 import Logo from "./Logo1.vue";
-import WishPage from "./WishPage.vue";
 
 export default {
   name: "LandingPage",
   components: {
     ourLogo: Logo,
-    wishPage: WishPage,
   },
 };
 </script>
 <style scoped>
+.container {
+  padding-top: 5vh;
+}
+.hlavni__obsah {
+  margin-top: 5vh;
+}
+
+h2 {
+  margin: 5vh 0;
+}
+
 p {
   text-align: center;
+  line-height: 1.3rem;
+}
+
+.claim {
+  font-size: 1.5rem;
+}
+
+.add__button {
+  margin-top: 5vh;
+  width: 20rem;
+  font-size: 1.2rem;
 }
 </style>
