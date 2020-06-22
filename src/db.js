7e3079e7625c09a3214344291fd1f232655066b2
Petra <zemape42@gmail.com>
2020-06-22 14:28:42 +0200

    Nova databaze

--- src/db.js
+++ src/db.js
@@ -2,13 +2,8 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyATS_mGgO9rpr-6-MV8CGDMe_rfyZMwbTA",
-  authDomain: "wishlist-5bd4e.firebaseapp.com",
-  databaseURL: "https://wishlist-5bd4e.firebaseio.com",
-  projectId: "wishlist-5bd4e",
-  storageBucket: "wishlist-5bd4e.appspot.com",
-  messagingSenderId: "66673050595",
-  appId: "1:66673050595:web:1bc94b5db51abdd262d6d6"
+  databaseURL: "https://wishlist2-2343f.firebaseio.com",
+  projectId: "wishlist2-2343f",
 };
 
 export default firebase.initializeApp(firebaseConfig).firestore();
