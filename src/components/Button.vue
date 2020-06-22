09df730b32f5891390d595c11a0f2d4873f48001
Jana Novotná <jana.posedelova@gmail.com>
2020-06-22 12:29:18 +0200

    práce s tlačítky v přání

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,16 +11,16 @@
       @click="changeTaken"
     >Zamluveno</button>
     <div
-      class="wish_button"
+      class="not_available"
       v-if="myList === false && anotherUser === true && taken === true"
     >Není k dispozici</div>
     <button
-      class="wish_button"
+      class="remove_wish_button"
       v-if="myList === true && taken === false"
       @click="deleteWish"
     >Smazat přání</button>
     <button
-      class="wish_button"
+      class="remove_wish_button"
       v-if="myList === true && taken === true"
       :disabled="false"
       @click="message"
@@ -60,8 +60,7 @@ export default {
   text-align: center;
   font-size: 1rem;
   background-color: rgba(89, 203, 188, 0.6);
-  color: rgb(71, 71, 71);
-  max-width: 60vw;
+  color: rgb(63, 62, 62);
   padding: 0.5rem;
   border: 0;
   border-radius: 5px;
@@ -72,5 +71,34 @@ export default {
   color: #fff;
   box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
     0 17px 50px 0 rgba(0, 0, 0, 0.19);
+}
+
+.not_available {
+  text-decoration: none;
+  text-align: center;
+  font-size: 1rem;
+  background-color: rgba(89, 203, 188, 0.6);
+  color: rgb(80, 79, 79);
+  padding: 0.5rem;
+  border: 0;
+  border-radius: 5px;
+}
+
+.remove_wish_button {
+  text-decoration: none;
+  text-align: center;
+  font-size: 1rem;
+  background-color: rgba(89, 203, 188, 0.6);
+  color: rgb(63, 62, 62);
+  padding: 0.5rem;
+  border: 0;
+  border-radius: 5px;
+  transition-duration: 0.4s;
+}
+
+.remove_wish_button:hover {
+  color: rgb(141, 44, 31);
+  box-shadow: 0 12px 16px 0 rgba(141, 44, 31, 0.24),
+    0 17px 50px 0 rgba(141, 44, 31, 0.19);
 }
 </style>
